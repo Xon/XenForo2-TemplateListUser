25afa6373ad44db653777494d6b316c859d8d851
Xon <tazipper1@hotmail.com>
2018-02-18 17:30:49 +0800

    Support user_id filter

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# XenForo2-TemplateListUser
+# TemplateListUser
+
 Fetch lists of users for templates
+
+## Usage
+```
+<xf:macro template="sv_list_users" 
+          name="info" 
+          arg-user_id="{{ [] }}" 
+          arg-secondary_group_ids="{{ [] }}" 
+          arg-not_secondary_group_ids="{{ [] }}" 
+          />
+```
--- upload/src/addons/SV/TemplateListUsers/Listener.php
+++ upload/src/addons/SV/TemplateListUsers/Listener.php
@@ -3,21 +3,33 @@
 
 namespace SV\TemplateListUsers;
 
-
 use XF\Searcher\User;
+use XF\Template\Templater;
 
-class Listener {
-	public static function templater_macro_pre_render (
-		\XF\Template\Templater $templater,
-		&$type,
-		&$template,
-		&$name,
-		array &$arguments,
-		array &$globalVars
-	){
-		/** @var User $searcher */
-		$searcher = \XF::app()->searcher('XF:User');
-		$searcher->setCriteria(array_intersect_key($arguments, array_flip(['secondary_group_ids', 'not_secondary_group_ids'])));
-		$arguments['users'] = $searcher->getFinder()->fetch();
-	}
+class Listener
+{
+    public static function templater_macro_pre_render(
+        /** @noinspection PhpUnusedParameterInspection */
+        Templater $templater,
+        &$type,
+        &$template,
+        &$name,
+        array &$arguments,
+        array &$globalVars
+    )
+    {
+        /** @var User $searcher */
+        $searcher = \XF::app()->searcher('XF:User');
+
+        $search = array_intersect_key($arguments, array_flip(['secondary_group_ids', 'not_secondary_group_ids', 'user_id']));
+        if ($search)
+        {
+            $searcher->setCriteria($search);
+            $arguments['users'] = $searcher->getFinder()->fetch();
+        }
+        else
+        {
+            $arguments['users'] = [];
+        }
+    }
 }
