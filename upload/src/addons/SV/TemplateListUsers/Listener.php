1b1e4177ba27002b937309e4bd17a40bdc146193
Xon <635541+Xon@users.noreply.github.com>
2025-04-27 20:46:52 +0800

    Reformat

--- upload/src/addons/SV/TemplateListUsers/Listener.php
+++ upload/src/addons/SV/TemplateListUsers/Listener.php
@@ -1,22 +1,14 @@
 <?php
 
-
 namespace SV\TemplateListUsers;
 
 use XF\Searcher\User;
 use XF\Template\Templater;
 
-class Listener
+abstract class Listener
 {
-    public static function templater_macro_pre_render(
-        /** @noinspection PhpUnusedParameterInspection */
-        Templater $templater,
-        &$type,
-        &$template,
-        &$name,
-        array &$arguments,
-        array &$globalVars
-    )
+    /** @noinspection PhpUnusedParameterInspection */
+    public static function templater_macro_pre_render(Templater $templater, string &$type, string &$template, string &$name, array &$arguments, array &$globalVars): void
     {
         /** @var User $searcher */
         $searcher = \XF::app()->searcher('XF:User');
