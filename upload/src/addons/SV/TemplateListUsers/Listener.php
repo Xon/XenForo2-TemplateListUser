ec4cd126c5b2d5c5df8c7a39239846e54f842fdd
Xon <635541+Xon@users.noreply.github.com>
2025-04-27 20:53:23 +0800

    simplify pass

--- upload/src/addons/SV/TemplateListUsers/Listener.php
+++ upload/src/addons/SV/TemplateListUsers/Listener.php
@@ -2,7 +2,7 @@
 
 namespace SV\TemplateListUsers;
 
-use XF\Searcher\User;
+use XF\Searcher\User as UserSearcher;
 use XF\Template\Templater;
 
 abstract class Listener
@@ -10,17 +10,15 @@ abstract class Listener
     /** @noinspection PhpUnusedParameterInspection */
     public static function templater_macro_pre_render(Templater $templater, string &$type, string &$template, string &$name, array &$arguments, array &$globalVars): void
     {
-        /** @var User $searcher */
-        $searcher = \XF::app()->searcher('XF:User');
+        $criteria = $arguments['criteria'] ?? [];
+        if (count($criteria) === 0)
+        {
+            return;
+        }
 
-        if (isset($arguments['criteria']))
-        {
-            $searcher->setCriteria($arguments['criteria']);
-            $arguments['users'] = $searcher->getFinder()->fetch();
-        }
-        else
-        {
-            $arguments['users'] = [];
-        }
+        /** @var UserSearcher $searcher */
+        $searcher = \XF::app()->searcher('XF:User'); // todo with XF2.3 only, use UserSearcher::class
+        $searcher->setCriteria($criteria);
+        $arguments['users'] = $searcher->getFinder()->fetch();
     }
 }
